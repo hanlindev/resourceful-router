426ca6c2de36ca8e6855257d5655fa3acba2d18b
Lin Han <hanlindev@fb.com>
2016-02-25 11:53:05 +0800

    Fix type definition

--- type-definition/resourceful-router.d.ts
+++ type-definition/resourceful-router.d.ts
@@ -2,18 +2,20 @@ declare module 'resourceful-router' {
     import col = require('resource-collection');
     import express = require('express');
 
-    function routerRegistrator(
-        router: express.Router,
-        globalPathPrefix: string
-    ): (
-        resourceName: string,
-        endpoint: col.ResourceEndpoint,
-        handlerModule: col.ResourceModule<col.ResourceAction>
-    ) => void;
+    module __ResourcefulRouter {
+        function routerRegistrator(
+            router: express.Router,
+            globalPathPrefix: string
+        ): (
+            resourceName: string,
+            endpoint: col.ResourceEndpoint,
+            handlerModule: col.ResourceModule<col.ResourceAction>
+        ) => void;
 
-    function getRouter(
-        resourceCollection: col.ResourceCollection<col.ResourceAction>
-    ): express.Router;
+        function getResourcefulRouter(
+            resourceCollection: col.ResourceCollection<col.ResourceAction>
+        ): express.Router;
+    }
 
-    export = getRouter;
+    export = __ResourcefulRouter;
 }
